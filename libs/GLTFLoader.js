7dfa84ca3e61c54e5d667be71a0931d9e4fc91b9
Eliot Destaing <eliot.destaing@epita.fr>
2025-11-12 10:22:47 +0100

    mano v2

--- libs/GLTFLoader.js
+++ libs/GLTFLoader.js
@@ -1,2 +1,4 @@
+import * as GLTFModule from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
+
+export default GLTFModule.GLTFLoader || GLTFModule.default || GLTFModule;
 export * from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
-export { default } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
