8a1e46d708dc8b756fdceb30780b95d3e2c95a3c
Eliot Destaing <eliot.destaing@epita.fr>
2025-11-12 10:21:38 +0100

    mano

--- libs/three.module.js
+++ libs/three.module.js
@@ -1,2 +1,4 @@
+import * as ThreeModule from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+
+export { ThreeModule as default };
 export * from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
-export { default } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
